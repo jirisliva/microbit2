ac30a2ac8e8bab867af2d5dab689a1a094f6a555
Jiří Slíva <jiri.sliva@gmail.com>
2021-12-23 23:48:40 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(music.volume() - 10)
+})
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(music.volume() + 10)
+})
+music.setVolume(12)
+loops.everyInterval(100, function () {
     led.plotBarGraph(
     input.lightLevel(),
     255
     )
-    music.ringTone(Math.map(input.lightLevel(), 0, 255, 500, 1000))
+    if (input.lightLevel() > 0) {
+        music.ringTone(Math.map(input.lightLevel(), 0, 255, 500, 1000))
+    }
 })
